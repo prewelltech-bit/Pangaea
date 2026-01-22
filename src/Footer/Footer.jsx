63fd50e5ee4a2f013fb74b06c0a46e3a0d75bd61
prewelltech-bit <prewelltech@gmail.com>
2026-01-22 13:57:31 +0530

    changes

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+
+    <>
+      <footer className="bg-gray-800 text-white py-6 mt-10">
+        <div className="container mx-auto text-center">
+          <p>&copy; {new Date().getFullYear()} My Company. All rights reserved.</p>
+        </div>
+      </footer>
+    </>
   )
 }
 
