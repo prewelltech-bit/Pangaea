53b71acf36b97a1a683fb9f6cd2b33f0164178f6
prewelltech-bit <prewelltech@gmail.com>
2026-01-23 17:51:20 +0530

    Resolve merge conflicts

--- src/component/home/Body.jsx
+++ src/component/home/Body.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 import Hero from './HomeBody/Section1/Hero';
-import SuccesStories from './HomeBody/Section2/SuccesStories';
-import Demo from './HomeBody/Section3/demo';
 import Welcome from './HomeBody/Section2/Welcome';
+import SuccessStories from './HomeBody/Section3/SuccessStories';
+import Demo from './HomeBody/Section4/demo';
+
 
 const Body = () => {
   return (
     <>
             <Hero/>
             <Welcome/>
-            <SuccesStories />
+            <SuccessStories />
             <Demo/>
 
     </>
--- src/component/home/HomeBody/Section2/Welcome.jsx
+++ src/component/home/HomeBody/Section2/Welcome.jsx
@@ -6,7 +6,22 @@ const Welcome = () => {
             <section className="welcome-section">
                 <div className="welcome-overlay">
                     <div className="welcome-card animate-welcome-card">
-                        <h1 className="welcome-h1">Find our events</h1>
+                        <h1 className="welcome-h1">Welcome To Pangaea</h1>
+
+                        <h3>Abroad Institute</h3>
+                        <p>
+                            is a Dallas Fort Worth, Texas based global agency for student
+                            advising, college preparation, and admission counseling. We advise
+                            both international and domestic students to assist college and
+                            university admission and recruitment teams with complete
+                            application packages for evaluation and decision making.
+                        </p>
+
+                        <p>
+                            Honesty, Integrity, and professional commitment are our company
+                            philosophy. We exceed the expectation of our students, partners,
+                            and educational institutions we partner with.
+                        </p>
                     </div>
                 </div>
             </section>
