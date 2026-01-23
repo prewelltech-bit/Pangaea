118d4a831262ea07a245f3479495f5510962e037
vanshreeshingala <vanshreeshingala2419@gmail.com>
2026-01-23 17:22:08 +0530

    welcome  v

--- src/component/home/Body.jsx
+++ src/component/home/Body.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import Hero from './HomeBody/Section1/Hero';
 import SuccessStories from './HomeBody/Section2/SuccessStories';
-import SuccesStories from './HomeBody/Section2/SuccesStories';
 import Demo from './HomeBody/Section3/demo';
+import Welcome from './HomeBody/Section4/Welcome';
 
 const Body = () => {
   return (
     <>
             <Hero/>
-        </div>
+            <Welcome />
+            <SuccessStories />
+            <Demo />
     </>
   )
 }
