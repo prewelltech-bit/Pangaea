00fc207765a38895ec0b1e2c4cca32a182c98aed
vanshreeshingala <vanshreeshingala2419@gmail.com>
2026-03-27 12:00:58 +0530

    fix folder issue

--- src/component/partnered-universities/PartneredUniversities.jsx
+++ src/component/partnered-universities/PartneredUniversities.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import Hero1 from './Section1/Hero1'
-import UniversitiesList from './Section2/UniversitiesList'
+import Hero1 from "./Section1/Hero1";
+import UniversitiesList from './Secttion2/UniversitiesList'
 
 const PartneredUniversities = () => {
   return (
     <>
-      <Hero1 />
-      <UniversitiesList />
+    <Hero1 />
+    <UniversitiesList />
     </>
   )
 }
