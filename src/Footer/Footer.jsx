9527df5c78f31d5c1f403cc2d9f6792aa3dbf284
prewelltech-bit <prewelltech@gmal.com>
2026-03-28 16:49:24 +0530

    changes B

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -23,7 +23,7 @@ const Footer = () => {
 
             {/* Brand */}
             <motion.div className="footer7-brand" variants={getDirectionVariants('left')}>
-              <img src="/public/assets/Logo/pp-2.png" alt="Pangaea Pathways Logo" className="footer7-logo" />
+              <img src="../../public/assets/Logo/PP-2.png" alt="Pangaea Pathways Logo" className="footer7-logo" />
             </motion.div>
 
             {/* Links */}
