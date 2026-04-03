1c69fde9d1778e2d9b7ca7b6dc1888f63eda64fd
prewelltech-bit <prewelltech@gmal.com>
2026-04-03 12:11:35 +0530

    Update vite.config.js with react plugin for proper build deployment

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,8 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default {
-  plugins: [tailwindcss()],
-};
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+});
